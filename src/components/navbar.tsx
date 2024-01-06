a9e14e1a4a5e5232dce876b3860dd03251d11ea9
rasuloff <asildev96@gmail.com>
2024-01-06 12:05:49 +0300

    icons updated

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -51,7 +51,7 @@ const Navbar = () => {
               <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
             </svg>
           </div>
-          <input type="search" id="default-search" className="block w-full h-11 p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-full outline-none bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Mockups, Logos..." required />
+          <input type="search" id="default-search" className="block w-full h-10 p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-full outline-none bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Mockups, Logos..." required />
         </div>
       </form>
 
@@ -63,30 +63,17 @@ const Navbar = () => {
             href={"/access"}
             className="flex justify-center items-center"
           >
-            <Image
-              src={"/sign-in.png"}
-              alt="sign in photo"
-              width={25}
-              height={25}
-              color="white"
-            />
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15M12 9l3 3m0 0-3 3m3-3H2.25" />
+            </svg>
+
           </Link>
 
-          {/* saqlanganlar */}
-          {/* <Link href={"/wishes"} className="flex justify-center items-center">
-            <Image src={"/like.png"}
-              width={22}
-              height={22}
-              alt="saqlangan mahsulotlar"
-            />
-          </Link> */}
           {/* savat */}
           <Link href={"/shopping-cart"} className="flex justify-center items-center">
-            <Image src={"/shopping-cart.png"}
-              alt="basket icon"
-              width={25}
-              height={25}
-            />
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
+            </svg>
           </Link>
 
           {/* Notifications */}
@@ -94,24 +81,19 @@ const Navbar = () => {
             className="flex justify-center items-center"
             href={"/notification"}
           >
-            <Image
-              src={"/notification.png"}
-              width={25}
-              height={25}
-              alt="notification icon"
-            />
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0" />
+            </svg>
           </Link>
 
           {/* User profile */}
           <Link className="flex justify-center items-center"
             href={"/profile"}
           >
-            <Image
-              src={"/user.png"}
-              width={25}
-              height={25}
-              alt="user profile"
-            />
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
+            </svg>
+
           </Link>
         </nav>
       </div>
