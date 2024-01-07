e61e80fb3eae1ad3b63c9e0639931b953d5df209
rasuloff <asildev96@gmail.com>
2024-01-07 14:09:32 +0300

    deals section updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,27 @@ export default async function Home() {
       <CatalogPage />
       <Hero />
       <section className='flex flex-col space-y-12 '>
-        <h1 className='text-5xl font-bold text-center'>Custom Shop Deal</h1>
+        <div className='flex items-center justify-between'>
+          <div className='flex items-center justify-start gap-2'>
+            <svg xmlns="http://www.w3.org/2000/svg" fill='none' viewBox="0 0 24 24" strokeWidth={1.5} stroke="orange" className="w-8 h-8">
+              <path strokeLinecap="round" strokeLinejoin="round" d="m3.75 13.5 10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75Z" />
+            </svg>
+            <p className='text-gray-800 text-2xl font-bold dark:text-white'>
+              Top Deals today
+            </p>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="orange" className="w-8 h-8">
+              <path strokeLinecap="round" strokeLinejoin="round" d="m3.75 13.5 10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75Z" />
+            </svg>
+          </div>
+          <div className='flex items-center justify-center transition-all text-blue-600 hover:underline dark:text-blue-300'>
+            <p className='font-medium text-sm'>Explore more</p>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
+              <path strokeLinecap="round" strokeLinejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
+            </svg>
+
+          </div>
+        </div>
+
         <div className='grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8'>
           {products.map((product) => (
             <Product key={product.id} product={product} />
--- src/components/catalog.tsx
+++ src/components/catalog.tsx
@@ -2,10 +2,10 @@ import Link from "next/link"
 
 const CatalogPage = () => {
     return (
-        <section className="flex w-full items-center justify-between  py-2 px-3 text-gray-900 text-sm font-semibold">
+        <section className="flex w-full flex-wrap items-center justify-between  py-2 px-3 text-gray-900 text-sm font-semibold">
             <Link href={"/deals"} className=" flex flex-col justify-center items-center gap-1">
-                <div className="flex justify-center items-center transition-all w-16 h-16 bg-blue-100 rounded-md hover:bg-blue-400 dark:bg-gray-800 dark:text-blue-200 dark:hover:bg-blue-300 dark:hover:text-gray-800">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-10 h-10">
+                <div className=" flex justify-center items-center transition-all w-16 h-16 bg-orange-100 rounded-md hover:bg-orange-200  dark:bg-orange-100 dark:text-orange-700">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" />
                     </svg>
                 </div>
@@ -78,8 +78,9 @@ const CatalogPage = () => {
             <Link href={"/travel"} className=" flex flex-col justify-center items-center gap-1">
                 <div className="flex justify-center items-center transition-all w-16 h-16 bg-blue-100 rounded-md hover:bg-blue-400 dark:bg-gray-800 dark:text-blue-200 dark:hover:bg-blue-300 dark:hover:text-gray-800">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5" />
+                        <path strokeLinecap="round" strokeLinejoin="round" d="m6.115 5.19.319 1.913A6 6 0 0 0 8.11 10.36L9.75 12l-.387.775c-.217.433-.132.956.21 1.298l1.348 1.348c.21.21.329.497.329.795v1.089c0 .426.24.815.622 1.006l.153.076c.433.217.956.132 1.298-.21l.723-.723a8.7 8.7 0 0 0 2.288-4.042 1.087 1.087 0 0 0-.358-1.099l-1.33-1.108c-.251-.21-.582-.299-.905-.245l-1.17.195a1.125 1.125 0 0 1-.98-.314l-.295-.295a1.125 1.125 0 0 1 0-1.591l.13-.132a1.125 1.125 0 0 1 1.3-.21l.603.302a.809.809 0 0 0 1.086-1.086L14.25 7.5l1.256-.837a4.5 4.5 0 0 0 1.528-1.732l.146-.292M6.115 5.19A9 9 0 1 0 17.18 4.64M6.115 5.19A8.965 8.965 0 0 1 12 3c1.929 0 3.716.607 5.18 1.64" />
                     </svg>
+
                 </div>
                 <p className="text-gray-900 transition-all hover:underline dark:text-blue-300">Travel</p>
             </Link>
